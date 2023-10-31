e426a0cbfcd5a7e48a13866eb4fef0143f5a03c0
w7dazs <135052332+Dabliu7@users.noreply.github.com>
2023-10-31 14:23:03 -0300

    Update pe.mk

--- pe.mk
+++ pe.mk
@@ -5,10 +5,7 @@ $(call inherit-product, device/custom/sepolicy/common/sepolicy.mk)
 
 BOARD_EXT4_SHARE_DUP_BLOCKS := true
 
-TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_BOOT_ANIMATION_RES := 720
 
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_SUPPORTS_CALL_RECORDING := true
-
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.system.ota.json_url=https://raw.githubusercontent.com/ponces/treble_build_pe/thirteen/ota.json
